295fd7d131b4968c7d0295770cf25f12e6da9da8
bayram3i <bayram.alhmd1998@gmail.com>
2026-04-13 23:40:37 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,24 +1,12 @@
-from telegram import Update
-from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
-import yt_dlp
+import telebot
 import os
 
-TOKEN = os.getenv("TOKEN")
+TOKEN = os.getenv("BOT_TOKEN")
 
-async def download(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    url = update.message.text
+bot = telebot.TeleBot(TOKEN)
 
-    try:
-        with yt_dlp.YoutubeDL({'format': 'best'}) as ydl:
-            info = ydl.extract_info(url, download=False)
-            video_url = info['url']
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, "البوت شغال ✅")
 
-        await update.message.reply_video(video=video_url)
-
-    except:
-        await update.message.reply_text("❌ الرابط غير مدعوم")
-
-app = ApplicationBuilder().token(TOKEN).build()
-app.add_handler(MessageHandler(filters.TEXT, download))
-
-app.run_polling()
+bot.infinity_polling()
